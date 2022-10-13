5bc3f53b5d93c224be14a2c53f872f214f113b2c
Irina <I.M.ZUBKOVA@GMAIL.COM>
2022-10-13 14:38:44 +0600

    add files

---
+++ Zadacha_50/Program.cs
@@ -0,0 +1,61 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1
+// 7 -> такого числа в массиве нет
+
+Console.Write("Число строк массива: ");
+int nSize = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число столбцов массива: ");
+int mSize = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[nSize, mSize];
+
+FillArray(array);
+PrintArray(array);
+
+Console.Write("Индекс строки элемента в массиве: ");
+int nIndex = Convert.ToInt32(Console.ReadLine());
+Console.Write("Индекс столбца элемента в массиве: ");
+int mIndex = Convert.ToInt32(Console.ReadLine());
+
+FindElemInArray(array, nIndex, mIndex);
+
+void FindElemInArray(double[,] matrixArray, int i, int j)
+{
+    if (i < 0 || i >= matrixArray.GetLength(0) ||
+        j < 0 || j >= matrixArray.GetLength(1))
+    {
+        Console.WriteLine("такого числа в массиве нет");
+    }
+    else
+        Console.WriteLine(matrixArray[i, j]);
+}
+
+
+void FillArray(double[,] matrixArray)
+{
+    Random rand = new Random();
+
+    for (int i = 0; i < matrixArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixArray.GetLength(1); j++)
+        {
+            matrixArray[i, j] = rand.Next(-10, 10);
+        }
+    }
+}
+
+void PrintArray(double[,] matrixArray)
+{
+    for (int i = 0; i < matrixArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixArray.GetLength(1); j++)
+        {
+            Console.Write($"{matrixArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Zadacha_52/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Число строк массива: ");
+int nSize = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число столбцов массива: ");
+int mSize = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[nSize, mSize];
+double [] columnsAvg = new double[mSize];
+
+FillArray(array);
+PrintArray(array);
+FindAverageInArrayColumns(array, columnsAvg);
+
+void FindAverageInArrayColumns(int[,] matrixArray, double[] cols)
+{
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    for (int i = 0, k = 0; i < matrixArray.GetLength(1); i++)
+    {
+        double sum = 0;
+        for (int j = 0; j < matrixArray.GetLength(0); j++)
+        {
+            sum += matrixArray[j, i];
+        }
+        cols[k] = sum / matrixArray.GetLength(0);
+        Console.Write($"{Math.Round(cols[k], 1)} ");
+        k++;
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[,] matrixArray)
+{
+    Random rand = new Random();
+
+    for (int i = 0; i < matrixArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixArray.GetLength(1); j++)
+        {
+            matrixArray[i, j] = rand.Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrixArray)
+{
+    for (int i = 0; i < matrixArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixArray.GetLength(1); j++)
+        {
+            Console.Write($"{matrixArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
